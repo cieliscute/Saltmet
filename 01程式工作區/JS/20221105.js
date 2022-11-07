36d9f7850f78fec0dd820447fc02b91ffda1ad40
forspe <60776436+cieliscute@users.noreply.github.com>
2022-11-07 12:01:39 +0800

    update

--- 01程式工作區/JS/20221105.js
+++ 01程式工作區/JS/20221105.js
@@ -1,2 +1,7 @@
 let mynav_active=document.querySelector('.mynav-active');
 let mynav_active_toggle_btn=document.querySelector('.navbar a.btn');
+
+mynav_active_toggle_btn.addEventListener('click',()=>{
+    mynav_active.classList.toggle('show');
+    console.log('1');
+})

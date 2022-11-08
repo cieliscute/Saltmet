0aef4d315a969324c6573dfd09b072e03033a083
cielforgithub <60776436+cieliscute@users.noreply.github.com>
2022-11-08 18:48:47 +0800

    update

--- 01程式工作區/JS/20221105.js
+++ 01程式工作區/JS/20221105.js
@@ -1,7 +1,15 @@
 let mynav_active=document.querySelector('.mynav-active');
-let mynav_active_toggle_btn=document.querySelector('.navbar a.btn');
+let mynav_active_toggle_btn=document.querySelector('#navbar-btn');
 
 mynav_active_toggle_btn.addEventListener('click',()=>{
     mynav_active.classList.toggle('show');
+    mynav_active_toggle_btn.classList.toggle('active');
     console.log('1');
+})
+
+let parallax_img=document.querySelector('.parallax img');
+window.addEventListener('scroll',()=>{
+    if(window.scrollY>500 && window.screenY<1000){
+        
+    }
 })
